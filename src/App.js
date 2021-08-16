f569078fad9ac0231da1531a5eff48a213bf9263
Muneeb Rasheed <mr.muneebrasheed@gmail.com>
2021-08-16 17:16:04 +0200

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import "./styles/global.css";
+import styled from "styled-components";
 import Box from "./components/Box";
 
 const tempMin = -20,
@@ -9,18 +10,33 @@ const tempMin = -20,
   heartMax = 180,
   stepsMin = 0,
   stepsMax = 50000;
+
+const StyledBox = styled(Box)`
+  color: black;
+`;
+
+const boxes = [
+  { icon: "local_drink", color: "#3A85FF", value: 1.5, unit: "L" },
+  { icon: "directions_walk", color: "black", value: 3000, unit: "steps" },
+  { icon: "favorite", color: "red", value: 120, unit: "bpm" },
+  { icon: "wb_sunny", color: "yellow", value: -10, unit: "°C" },
+];
 export class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
           <p>Heart: {heartMin}</p>
-          <p>Temprature {tempMin}</p>
+          <p>Temprature: {tempMin}</p>
           <p>Box: {stepsMin}</p>
-          <Box icon="local_drink" color="#3A85FF" value={1.5} unit="L" />
-          <Box icon="directions_walk" color="black" value={3000} unit="steps" />
-          <Box icon="favorite" color="red" value={120} unit="bpm" />
-          <Box icon="wb_sunny" color="yellow" value={-10} unit="°C" />
+          {boxes.map((box) => (
+            <Box
+              icon={box.icon}
+              color={box.color}
+              value={box.value}
+              unit={box.unit}
+            />
+          ))}
         </div>
       </div>
     );
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,8 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+const StyledBox = styled.div`
+  &:nth-child(n) {
+    background: #aaaaaa;
+  }
+  &:nth-child(2n) {
+    background: #cccccc;
+  }
+  & {
+    height: 190px;
+    padding: 8px;
+    text-align: center;
+  }
+`;
 class Box extends React.Component {
   render() {
     return (
-      <div className="box col-sm-3 col-6">
+      <StyledBox className="col-sm-3 col-6">
         <span
           class="material-icons"
           style={{ fontSize: 100, color: `${this.props.color}` }}
@@ -12,7 +26,7 @@ class Box extends React.Component {
         <p>
           {this.props.value} : {this.props.unit}
         </p>
-      </div>
+      </StyledBox>
     );
   }
 }
